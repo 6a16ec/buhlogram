eddc7bc03e4496cd80f2445f1f814565ed27e3f3
6a16ec <semaev2014@yandex.ru>
2018-05-26 21:42:25 +0300

    Update MainActivity.java

--- base/MainActivity.java
+++ base/MainActivity.java
@@ -5,18 +5,17 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager sensorManager; //менеджер сенсоров
 
-    private float[] rotationMatrix; //матрица поворота
 
-    private float[] accelerometer;  //данные с акселерометра
-    private float[] geomagnetism;   //данные геомагнитного датчика
-    private float[] orientation;    //матрица положения в пространстве
+
+    MathModel model = new MathModel(100, 100, 100);
+    Sensors sensors = new Sensors();
 
 
     public MainActivity() {
@@ -29,10 +28,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
 
 
-        rotationMatrix = new float[16];
-        accelerometer = new float[3];
-        geomagnetism = new float[3];
-        orientation = new float[3];
 
 
 
@@ -41,23 +36,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        loadSensorData(event); // получаем данные с датчика
-        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometer, geomagnetism); //получаем матрицу поворота
-        SensorManager.getOrientation(rotationMatrix, orientation); //получаем данные ориентации устройства в пространстве
-//
-//        if((xyAngle ==null)||(xzAngle==null)||(zyAngle ==null)){
-//            xyAngle = (TextView) findViewById(R.id.xyValue);
-//            xzAngle = (TextView) findViewById(R.id.xzValue);
-//            zyAngle = (TextView) findViewById(R.id.zyValue);
-//        }
 
-        String xy, xz, zy;
-        xy = String.valueOf(Math.round(Math.toDegrees(orientation[0])));
-        xz = String.valueOf(Math.round(Math.toDegrees(orientation[1])));
-        zy = String.valueOf(Math.round(Math.toDegrees(orientation[2])));
-//        //вывод результата
-//
-        TextView text;text = (TextView) findViewById(R.id.text);text.setText(xy + " " + xz + " " + zy);
+        sensors.sensorChanged(event);
+
+        if(sensors.haveNewInfo()){
+            TextView text;text = (TextView) findViewById(R.id.text);text.setText(sensors.getString());
+        }
+
+
+
     }
 
 
@@ -81,15 +68,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager.unregisterListener(this);
     }
 
-    private void loadSensorData(SensorEvent event) {
-        final int type = event.sensor.getType(); //определяем тип датчика
-        if (type == Sensor.TYPE_ACCELEROMETER) { //если акселерометр
-            accelerometer = event.values.clone();
-        }
 
-        if (type == Sensor.TYPE_MAGNETIC_FIELD) { //если геомагнитный датчик
-            geomagnetism = event.values.clone();
-        }
-    }
 }
 
