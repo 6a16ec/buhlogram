84f1a2be5bdae3544d5e366440115a8eb0656709
dlapin2014 <30550066+dlapin2014@users.noreply.github.com>
2018-06-05 20:29:41 +0300

    Add files via upload

--- main/MainActivity.java
+++ main/MainActivity.java
@@ -1,185 +1,58 @@
-package com.example.user.lapin;
+package com.example.user.myapplication;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
+import android.content.Context;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.util.JsonReader;
-import android.util.Log;
-import android.view.ViewGroup.LayoutParams;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity {
 
+    SeekBar seekBar;
+    TextView textView;
+    ImageButton startBut;
 
-public class MainActivity extends Activity implements SensorEventListener {
-    int counter = 0;
-    int diapason = 50;
-
-    Sensors sensors = new Sensors();
-    GPS gps = new GPS();
-
-
-    UpdateInformation updater;
-
-
-
-
-
-    MathModel model = new MathModel();
-
-    Preview mPreview;
-    DrawOnTop mDraw;
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        sensors.onCreate(this);
-        gps.onCreate(this);
-
-
-
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-        mPreview = new Preview(this);
-        mDraw = new DrawOnTop(this);
-
-        setContentView(mPreview);
-        addContentView(mDraw, new LayoutParams
-                (LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        textView = (TextView)findViewById(R.id.textView);
+        startBut = (ImageButton)findViewById(R.id.imageButton);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
 
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
-
-
-//        model.addPlane("# # 0 0 0 100 300 # # A320 # # MSK NN # # # # # LOL", 0 , 0 , 0 );
-        ArrayList plane = new ArrayList();
-
-        for(int i = 0; i < 20; i++)
-            plane.add(new Object());
-
-        plane.add(1, 0.001);
-        plane.add(2, 0.001);
-        plane.add(3, 0);
-        plane.add(4, 300);
-        plane.add(5, 500);
-        plane.add(8, "A320");
-        plane.add(11, "MSK");
-        plane.add(12, "NN");
-        plane.add(18, "LOL");
-        model.addPlane(new Plane(plane, 0, 0 ,0));
-//        model.addPlane("# # 0 0 0 100 300 # # A320 # # MSK NN # # # # # LOL", 0 , 0 , 0 );
-//
-        flyInfoTimer.start();
-
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        sensors.onResume(this);
-        gps.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        sensors.onPause(this);
-        gps.onPause();
-    }
-
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-//
-//        if(gps.isNewInfo()) {
-//            updater = new UpdateInformation(gps.getLatitude(), gps.getLongitude(), 50);
-//            Log.e("njvgkjkfvbv vbndfjvnfj", String.valueOf(gps.getLatitude()) +  " " + String.valueOf(gps.getLongitude() +  " " + String.valueOf(gps.getAltitude())));
-//            gps.setNewInfo(false);
-//        }
-
-//        if(updater != null) Log.e("njdkvnsjkvnjdfv", String.valueOf(updater.isNewInfo()));
-
-
-        if(updater != null && updater.isNewInfo()){
-            Log.e("-----mdfklbvnfdklb---", String.valueOf(updater.getAmount()));
-            Log.e("Link", updater.request);
-            model.clearArray();
-            for(int i = 0; i < updater.getAmount(); i++){
-                model.addPlane(new Plane(updater.getPlane(i), gps.getLatitude(), gps.getLongitude(), gps.getAltitude()));
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView.setText(String.valueOf(i+20)+"km");
             }
-            updater.setNewInfo(false);
-        }
 
-        sensors.checkSensors(event);
-
-        if(sensors.haveNewInfo()){
-
-            model.check(sensors.getXY(), sensors.getXZ(), sensors.getZY());
-            mDraw.planes = new ArrayList<Plane>();
-            for(int i = 0; i < model.amount(); i++){
-                if(model.isOnScreen(i))
-                    mDraw.planes.add(model.plane(i));
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
-            mDraw.invalidate();
 
-//            Log.e("0", String.valueOf(sensors.getXY()) + " " + String.valueOf(sensors.getXZ()) + " " + String.valueOf(sensors.getZY()));
-        }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        startBut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(((LocationManager)getSystemService(Context.LOCATION_SERVICE)).isProviderEnabled(LocationManager.GPS_PROVIDER))
+                {}
+                else
+                    Toast.makeText(getApplicationContext(), "Please, enable GPS, we can't work without it", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //используется для получения уведомлений от SensorManager при изменении значений датчика
-    }
-
-
-
-
-
-    CountDownTimer flyInfoTimer = new CountDownTimer(Integer.MAX_VALUE, 10000){
-
-
-        int i = 0;
-        @Override
-        public void onTick(long l) {
-
-            updater = new UpdateInformation(gps.getLatitude(), gps.getLongitude(), diapason);
-            updater.execute();
-
-//            UpdateInformation updater = new UpdateInformation(56, 44, 50);
-//            while(!updater.isNewInfo());
-//            model.clearArray();
-//            for(int i = 0; i < updater.getAmount(); i++){
-//                model.addPlane(new Plane(updater.getPlane(i), 56, 44, 100));
-//            }
-//            Log.d("nTIIIMEEE-----", "hello" + String.valueOf(counter));
-//            counter += 1;
-//            JsonReader jsonReader = new JsonReader();
-//            jsonReader.
-        }
-
-        @Override
-        public void onFinish() {
-
-        }
-    };
-
-
-
 }
